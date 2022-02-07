d4d50d093620197c7ba31ceb360ac0d5c7622220
Joe Keenan <joe@flyingdiver.com>
2022-02-06 20:11:32 -0500

    Python3 Update

--- TankUtility.indigoPlugin/Contents/Server Plugin/plugin.py
+++ TankUtility.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -8,6 +8,7 @@ import logging
 
 BASE_URL = "https://data.tankutility.com/api/"
 
+
 ################################################################################
 class Plugin(indigo.PluginBase):
 
@@ -19,33 +20,28 @@ class Plugin(indigo.PluginBase):
 
         pfmt = logging.Formatter('%(asctime)s.%(msecs)03d\t[%(levelname)8s] %(name)20s.%(funcName)-25s%(msg)s', datefmt='%Y-%m-%d %H:%M:%S')
         self.plugin_file_handler.setFormatter(pfmt)
-
-        try:
-            self.logLevel = int(self.pluginPrefs[u"logLevel"])
-        except:
-            self.logLevel = logging.INFO
+        self.logLevel = int(self.pluginPrefs.get("logLevel", logging.INFO))
         self.indigo_log_handler.setLevel(self.logLevel)
-        self.logger.debug(u"logLevel = " + str(self.logLevel))
-    
+        self.logger.debug(f"logLevel = {self.logLevel}")
+
+        self.tuDevices = {}
+        self.securityToken = None
 
     def startup(self):
-        self.logger.info(u"Starting TankUtility")
-        self.tuDevices = {}
-
+        self.logger.info("Starting TankUtility")
 
     def shutdown(self):
-        self.logger.info(u"Shutting down TankUtility")
-
+        self.logger.info("Shutting down TankUtility")
 
     def deviceStartComm(self, device):
 
-        self.logger.debug("deviceStartComm: Adding Device %s (%d) to TankUtility device list" % (device.name, device.id))
+        self.logger.debug(f"deviceStartComm: Adding Device {device.name} ({device.id:d}) to TankUtility device list")
         assert device.id not in self.tuDevices
         self.tuDevices[device.id] = device
         device.stateListOrDisplayStateIdChanged()
-        
+
     def deviceStopComm(self, device):
-        self.logger.debug("deviceStopComm: Removing Device %s (%d) from TankUtility device list" % (device.name, device.id))
+        self.logger.debug(f"deviceStopComm: Removing Device {device.name} ({device.id:d}) from TankUtility device list")
         assert device.id in self.tuDevices
         del self.tuDevices[device.id]
 
@@ -57,155 +53,133 @@ class Plugin(indigo.PluginBase):
         self.logger.debug(u"validatePrefsConfigUi called")
         errorDict = indigo.Dict()
 
-        try:
-            self.logLevel = int(valuesDict[u"logLevel"])
-        except:
-            self.logLevel = logging.INFO
-        self.indigo_log_handler.setLevel(self.logLevel)
-        self.logger.debug(u"logLevel = " + str(self.logLevel))
-
         if len(valuesDict['tuLogin']) < 5:
-            errorDict['tuLogin'] = u"Enter your TankUtility login name (email address)"
+            errorDict['tuLogin'] = "Enter your TankUtility login name (email address)"
 
         if len(valuesDict['tuPassword']) < 1:
-            errorDict['tuPassword'] = u"Enter your TankUtility login password"
+            errorDict['tuPassword'] = "Enter your TankUtility login password"
 
         if len(errorDict) > 0:
-            return (False, valuesDict, errorDict)
-
-        return (True, valuesDict)
-
+            return False, valuesDict, errorDict
+        return True, valuesDict
 
     def closedPrefsConfigUi(self, valuesDict, userCancelled):
         if not userCancelled:
-            try:
-                self.logLevel = int(valuesDict[u"logLevel"])
-            except:
-                self.logLevel = logging.INFO
+            self.logLevel = int(valuesDict.get("logLevel", logging.INFO))
             self.indigo_log_handler.setLevel(self.logLevel)
-            self.logger.debug(u"logLevel = " + str(self.logLevel))
+            self.logger.debug(f"logLevel = {self.logLevel}")
 
     ########################################
 
     def actionControlSensor(self, action, dev):
 
         if action.sensorAction == indigo.kDeviceAction.RequestStatus:
-            self.logger.debug(u"actionControlSensor: \"%s\" Request Status" % dev.name)
+            self.logger.debug(f"actionControlSensor: \"{dev.name}\" Request Status")
             self.getDevices()
-
         else:
-            self.logger.error(u"actionControlSensor: \"%s\" Unsupported action requested: %s" % (dev.name, str(action)))
-
+            self.logger.error(f"actionControlSensor: \"{dev.name}\" Unsupported action requested: {action}")
 
     ########################################
 
-
     def tuLogin(self, username=None, password=None):
 
-        if username == None or password == None:
-            self.logger.debug(u"tuLogin failure, Username or Password not set")
+        if username is None or password is None:
+            self.logger.debug("tuLogin failure, Username or Password not set")
             return False
 
         url = BASE_URL + 'getToken'
         try:
             response = requests.get(url, auth=(username, password), verify=False)
         except requests.exceptions.RequestException as err:
-            self.logger.debug(u"tuLogin failure, request url = %s" % (url))
-            self.logger.error(u"tuLogin failure, RequestException: %s" % (str(err)))
+            self.logger.debug(f"tuLogin failure, request url = {url}, RequestException: {str(err)}")
             self.securityToken = ""
             return False
 
-        if (response.status_code != requests.codes.ok):
-            self.logger.debug(u"tuLogin failure, Enum err code %s" % (response.status_code))
+        if response.status_code != requests.codes.ok:
+            self.logger.debug(f"tuLogin failure, Enum err code {response.status_code}")
             self.securityToken = ""
-            return False        
+            return False
 
         try:
             data = response.json()
-        except:
-            self.logger.error(u"tuLogin failure, JSON Decode Error")
+        except (Exception,):
+            self.logger.error("tuLogin failure, JSON Decode Error")
             self.securityToken = ""
             return False
 
         self.securityToken = data['token']
-        self.logger.debug(u"tuLogin successful")
+        self.logger.debug("tuLogin successful")
         return True
 
     ########################################
 
     def getDevices(self):
 
-        self.logger.info(u"Getting tank data from TankUtility server...")
-        
+        self.logger.info("Getting tank data from TankUtility server...")
+
         if not self.tuLogin(self.pluginPrefs.get('tuLogin', None), self.pluginPrefs.get('tuPassword')):
-            self.logger.debug(u"getDevices: TankUtility Login Failure")
+            self.logger.debug("getDevices: TankUtility Login Failure")
             return
 
-        url =  BASE_URL + 'devices'
-        params = { 'token':self.securityToken }
+        tank_dev = None
+        url = BASE_URL + 'devices'
+        params = {'token': self.securityToken}
         response = requests.get(url, params=params, verify=False)
-        response.raise_for_status
-        
+        response.raise_for_status()
+
         devices_data = response.json()
 
         for tuDevice in devices_data['devices']:
-        
-            url =  BASE_URL + 'devices/' + tuDevice
-            params = { 'token':self.securityToken }
+
+            url = BASE_URL + 'devices/' + tuDevice
+            params = {'token': self.securityToken}
             response = requests.get(url, params=params, verify=False)
-            response.raise_for_status
+            response.raise_for_status()
 
             tank_data = response.json()
-            self.logger.debug(u"getDevices: Tank {} data =\n{}\n".format(tuDevice, tank_data))
-
-            keyValueList = []
+            self.logger.debug(f"getDevices: Tank {tuDevice} data =\n{tank_data}\n")
 
             found = False
             iterator = indigo.devices.iter(filter="self")
-            for dev in iterator:
-                if dev.address == tuDevice:
+            for tank_dev in iterator:
+                if tank_dev.address == tuDevice:
                     found = True
-                    tank_dev = dev
                     break
-                    
+
             if not found:
-                self.logger.debug(u'Unknown TankUtility Device: %s' % (tuDevice))
+                self.logger.debug(f'Unknown TankUtility Device: {tuDevice}')
 
                 try:
                     tank_dev = indigo.device.create(protocol=indigo.kProtocol.Plugin,
-                        address=tuDevice,
-                        description = "Tank Sensor Device auto-created by TankUtility plugin",
-                        deviceTypeId='tankSensor',
-                        props={ 'AllowOnStateChange': False,
-                                'SupportsOnState': False,
-                                'SupportsSensorValue': True,
-                                'SupportsStatusRequest': True
-                            },
-                        name="TankUtility " + tuDevice)
+                                                    address=tuDevice,
+                                                    description="Tank Sensor Device auto-created by TankUtility plugin",
+                                                    deviceTypeId='tankSensor',
+                                                    props={'AllowOnStateChange': False,
+                                                           'SupportsOnState': False,
+                                                           'SupportsSensorValue': True,
+                                                           'SupportsStatusRequest': True
+                                                           },
+                                                    name="TankUtility " + tuDevice)
                 except Exception as err:
-                    self.logger.error(u'Error Creating Sensor Device: %s' % (tuDevice))
+                    self.logger.error(f'Error Creating Sensor Device: {tuDevice}')
                     continue
 
-                keyValueList.append({'key': 'owner_name', 'value':   tank_data['device']['name']})
-                keyValueList.append({'key': 'tank_address', 'value': tank_data['device']['address']})
-                keyValueList.append({'key': 'capacity', 'value':     tank_data['device']['capacity']})
-                keyValueList.append({'key': 'fuel_type', 'value':    tank_data['device']['fuelType']})
-
+            keyValueList = [{'key': 'owner_name', 'value': tank_data['device']['name']},
+                            {'key': 'tank_address', 'value': tank_data['device']['address']},
+                            {'key': 'capacity', 'value': tank_data['device']['capacity']},
+                            {'key': 'fuel_type', 'value': tank_data['device']['fuel_type']}]
 
             tank = tank_data['device']['lastReading']['tank']
-            tankStr = "{:.2f} %".format(tank)
-            keyValueList.append({'key': 'sensorValue', 'value': tank, 'uiValue': tankStr})
+            keyValueList.append({'key': 'sensorValue', 'value': tank, 'uiValue': f"{tank:.2f} %"})
 
             temperature = tank_data['device']['lastReading']['temperature']
-            temperatureStr = "{:.1f} °F".format(temperature)
-            keyValueList.append({'key': 'temperature', 'value': temperature, 'uiValue':temperatureStr})
+            keyValueList.append({'key': 'temperature', 'value': temperature, 'uiValue': f"{temperature:.1f} °F"})
 
             last_update = float(tank_data['device']['lastReading']['time']) / 1000.0
             timeStr = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(last_update))
             keyValueList.append({'key': 'last_update', 'value': timeStr})
 
             tank_dev.updateStatesOnServer(keyValueList)
-
 
     ########################################
 
@@ -214,57 +188,53 @@ class Plugin(indigo.PluginBase):
 
     def doDailyAction(self, pluginAction):
 
-        self.getDevices()   # get an update
-                
+        self.getDevices()  # get an update
+
         iterator = indigo.devices.iter(filter="self.tankSensor")
         for dev in iterator:
-            self.logger.info(u"{}: Performing daily calculations...".format(dev.name))
+            self.logger.info(f"{dev.name}: Performing daily calculations...")
 
             state_list = []
 
             previous_reading = float(dev.states['previous_reading'])
-            self.logger.debug(u"doDaily: previous reading {:.2f} %".format(previous_reading))
-            
+            self.logger.debug(f"doDaily: previous reading {previous_reading:.2f} %")
+
             current_reading = float(dev.sensorValue)
-            self.logger.debug(u"doDaily: current reading {:.2f} %".format(current_reading))
-            
+            self.logger.debug(f"doDaily: current reading {current_reading:.2f} %")
+
             if current_reading > previous_reading:
-                self.logger.debug(u"doDaily: Tank refilled, resetting")
+                self.logger.debug("doDaily: Tank refilled, resetting")
                 usage = 0.0
 
-            else:                
+            else:
                 usage = ((previous_reading - current_reading) / 100.0) * float(dev.states['capacity'])
- 
-            self.logger.debug(u"doDaily: Daily usage {:.2f} gallons".format(usage))
+
+            self.logger.debug(f"doDaily: Daily usage {usage:.2f} gallons")
 
             current_month_usage = float(dev.states['current_month_usage'])
-            self.logger.debug(u"doDaily: Current month usage {:.2f} gallons".format(current_month_usage))
-                   
+            self.logger.debug(f"doDaily: Current month usage {current_month_usage:.2f} gallons")
+
             state_list.append({'key': 'daily_usage', 'value': usage})
             state_list.append({'key': 'previous_reading', 'value': current_reading})
             state_list.append({'key': 'current_month_usage', 'value': (current_month_usage + usage)})
             dev.updateStatesOnServer(state_list)
-       
+
     def doMonthlyAction(self, pluginAction):
-                
+
         iterator = indigo.devices.iter(filter="self.tankSensor")
         for dev in iterator:
-            self.logger.info(u"{}: Performing monthly calculations...".format(dev.name))
+            self.logger.info(f"{dev.name}: Performing monthly calculations...")
 
-            state_list = []
-            
             monthly_reading = float(dev.states['monthly_reading'])
-            self.logger.debug(u"doMonthly: previous monthly reading {:.2f} %".format(monthly_reading))
-            
+            self.logger.debug(f"doMonthly: previous monthly reading {monthly_reading:.2f} %")
+
             current_reading = float(dev.sensorValue)
-            self.logger.debug(u"doMonthly: current reading {:.2f} %".format(current_reading))
-            
+            self.logger.debug(f"doMonthly: current reading {current_reading:.2f} %")
+
             monthly_usage = float(dev.states['current_month_usage'])
-            
-            self.logger.debug(u"doMonthly: Monthly usage {:.2f} gallons".format(monthly_usage))
-                    
-            state_list.append({'key': 'monthly_reading', 'value': current_reading})
-            state_list.append({'key': 'monthly_usage', 'value': monthly_usage})
-            state_list.append({'key': 'current_month_usage', 'value': 0.0})
+            self.logger.debug(f"doMonthly: Monthly usage {monthly_usage:.2f} gallons")
+
+            state_list = [{'key': 'monthly_reading', 'value': current_reading},
+                          {'key': 'monthly_usage', 'value': monthly_usage},
+                          {'key': 'current_month_usage', 'value': 0.0}]
             dev.updateStatesOnServer(state_list)
-       
